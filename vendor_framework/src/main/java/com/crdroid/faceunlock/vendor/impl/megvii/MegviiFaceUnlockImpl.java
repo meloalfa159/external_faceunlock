f6741053affcf90229e88c5568276741796c618b
melo159 <melolongo87@gmail.com>
2023-04-24 17:58:35 +0200

    initialize

---
+++ vendor_framework/src/main/java/com/crdroid/faceunlock/vendor/impl/megvii/MegviiFaceUnlockImpl.java
@@ -0,0 +1,17 @@
+package com.crdroid.faceunlock.vendor.impl.megvii;
+
+import com.megvii.facepp.sdk.Lite;
+
+public class MegviiFaceUnlockImpl extends Lite {
+    private static MegviiFaceUnlockImpl sInstance;
+
+    private MegviiFaceUnlockImpl() {
+    }
+    
+    public static MegviiFaceUnlockImpl getInstance() {
+        if (sInstance == null) {
+            sInstance = new MegviiFaceUnlockImpl();
+        }
+        return sInstance;
+    }
+}
---
+++ vendor_framework/src/main/java/faceunlock/util/FaceUnlockEncryptorDependencyLib.java
@@ -0,0 +1,6 @@
+package faceunlock.util;
+
+import com.crdroid.faceunlock.vendor.utils.CustomUnlockEncryptor;
+
+public class FaceUnlockEncryptorDependencyLib extends CustomUnlockEncryptor {
+}
